68c62a75ce66f907a17f0a1d44511594be97bd6e
jianrontan <116509027+jianrontan@users.noreply.github.com>
2026-02-21 22:06:09 +0800

    Updated GUI and added some personality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -16,10 +17,21 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
+            // Load background image to get dimensions
+            Image backgroundImage = new Image(this.getClass().getResourceAsStream("/images/Motherland.png"));
+            double imageWidth = backgroundImage.getWidth();
+            double imageHeight = backgroundImage.getHeight();
+
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
+
+            // Set AnchorPane size to match image
+            ap.setPrefWidth(imageWidth);
+            ap.setPrefHeight(imageHeight);
+
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("KirkStein");
             fxmlLoader.<MainWindow>getController().setKirkStein(kirkStein);
             stage.show();
         } catch (IOException e) {
